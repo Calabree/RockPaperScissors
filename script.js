2db21902545654ff8be66eb96f7f460761f00a43
Peter Calabrese <peter@thirtyfour.tech>
2021-04-25 12:00:02 -0400

    cleaned up scoring system into func. Added winning logic

--- script.js
+++ script.js
@@ -20,18 +20,17 @@ function playGame(value){
             }
             else if(x==1){
                 window.alert("Computer picked paper! You lose this round!");
-                computerScore++;
-                document.getElementById('compScore').innerHTML = "Computer Score: " + computerScore;
+                computerScore=updateCompScore(computerScore);
             }
             else if(x==2){
                 window.alert("Computer picked scissors! You win this round!");
-                userScore++;
-                document.getElementById('userScore').innerHTML = "Your Score" + userScore;            }
+                userScore=updateUserScore(userScore);
+                }
             break;
         case "paper":
             if(x==0){
                 window.alert("Computer picked rock! You win this round!");
-               
+                userScore=updateUserScore(userScore);
             }
             else if(x==1){
                 window.alert("Computer picked paper! You tied this round!");
@@ -39,26 +38,46 @@ function playGame(value){
             }
             else if(x==2){
                 window.alert("Computer picked scissors! You lose this round!");
-                computerScore++;
-                document.getElementById('compScore').innerHTML = "Computer Score: " + computerScore;
+                computerScore=updateCompScore(computerScore);
             }
             break;
         case "scissors":
             if(x==0){
                 window.alert("Computer picked rock! You lose this round!");
-                computerScore++;
-                document.getElementById('compScore').innerHTML = "Computer Score: " + computerScore;
+                computerScore=updateCompScore(computerScore);
+
             }
             else if(x==1){
                 window.alert("Computer picked paper! You win this round!");
-                userScore++; 
-                document.getElementById('userScore').innerHTML = "Your Score" + userScore;  
+                userScore=updateUserScore(userScore);
             }
             else if(x==2){
                 window.alert("Computer picked scissors! You tie this round!");
             }
             break;
     }
+
+    winCheck(userScore,computerScore);
     
-    
+}
+
+function winCheck(Usrscr, cmpsr){
+    if (Usrscr == 5){
+        window.alert("You Win, Press OK to reload page.");
+        window.location.reload();
+    }
+    if (cmpsr == 5){
+        window.alert("You Lose, Press OK to reload page.");
+        window.location.reload();
+    }
+}
+function updateUserScore(score){
+    score++;
+    document.getElementById('userScore').innerHTML = "Your Score: " + score; 
+    return score;
+}
+function updateCompScore(score){
+    score++;
+    document.getElementById('compScore').innerHTML = "Computer Score: " + score;
+    return score;
 }
